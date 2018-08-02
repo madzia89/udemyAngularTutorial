8f84d6036b8ac15af07c4769d8246a38ede97274
Magda Wójcicka <wojcickam@tt.com.pl>
2018-08-02 12:55:55 +0200

    implementacja interfejsu w klasie

--- TsSandbox/klasy1.ts
+++ TsSandbox/klasy1.ts
@@ -1,12 +1,9 @@
-class Person1{
-    public name: string; 
-    //dodanie public jest równoznaczne z nie używaniem modyfikatorów dostępu,
-    // public jest nadawany domyślnie. w age: number poniżej "public" zostało
-    // dodane domyślnie pomimo że tego nie widzimy
-    age: number;
-    constructor(name: string, age: number){
-        this.name = name;
-        this.age = age;
+interface canTalk {
+    talk(): string
+}// każda metoda która implementuje ten interface musi posiadać metodę talk
+
+class Person1 implements canTalk{
+    constructor(public name: string, public age: number){
     }
 
     welcome(){
@@ -20,14 +17,15 @@ class Person1{
             return false
         }
     }
+
+    talk(){
+        return "talk"
+    }
 }
 
 class Customer4 extends Person1{
-    advisor: string;
-
-    constructor(name: string, age: number, advisor: string){
+    constructor(name: string, age: number, private advisor: string){
         super(name, age) 
-        this.advisor = advisor
     }
 
     welcome(){
