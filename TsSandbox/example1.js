f12412a6788e83124fefea64275e4584efa4f902
Magda Wójcicka <wojcickam@tt.com.pl>
2018-08-02 08:55:55 +0200

    create js file

---
+++ TsSandbox/example1.js
@@ -0,0 +1,6 @@
+console.log("hello world");
+var x = 3;
+var xs = [1, 2, 3, 4, 5, 6]; // === let xs: Array<number>
+console.log(xs.length);
+var zs = [9, 8, 7].concat(xs, [52]);
+console.log(zs); // wyświetli dane zs oraz elementy z xs
--- TsSandbox/example1.ts
+++ TsSandbox/example1.ts
@@ -4,7 +4,25 @@ let x: number = 3;
 
 let xs: number[] = [1,2,3,4,5,6]; // === let xs: Array<number>
 
-console.log(xs.length);
+// console.log(xs.length);
 
 let zs = [9,8,7, ... xs, 52]
-console.log(zs)// wyświetli dane zs oraz elementy z xs
+// console.log(zs)// wyświetli dane zs oraz elementy z xs
+
+function welcome(name: string, age?: number): string { 
+    //znak zapytania przy age oznacza, że jest to argument który jest opcjonalny tj. może, ale nie musi się pojawić
+    //po drugim dwukropku możemy wskazać co ma zwracać funkcja, tylko wtedy funkcja musi coś zwracać a nie być np. sam console.log
+    return `Welcome ${name}`
+}
+
+function welcome2(name: string, age: number = 25): string { 
+    // 'age:number = 25' oznacza, że jeżeli nie zostanie podany argument age, wówczas funkcja ma domyślnie przyjąć, że jest to 25
+      return `Welcome ${name}`
+  }
+  
+function welcome3(...names: string[]): string { 
+// ... ten spread pozwala użytkownikowi na podanie wielu imion, które zapiszą się w tablicy i będą stringami
+      return `Welcome ${names.join(', ')}`
+  }
+
+  console.log(welcome3('Magda', 'Iwonka', 'Wiola'))
