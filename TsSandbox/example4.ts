b3ec046e767117aa0711726acfb99849754cb7ce
Magda Wójcicka <wojcickam@tt.com.pl>
2018-08-02 10:40:33 +0200

    inny sposób stworzenia obiektu z klasy

--- TsSandbox/example4.ts
+++ TsSandbox/example4.ts
@@ -11,11 +11,13 @@
 class Customer1{
     name: string;
     age: number;
-}
 
-let customerek = new Customer1();
-customerek.name = "Ola"
-customerek.age = 32
+    constructor(name: string, age: number){
+        this.name = name
+        this.age = age
+    }
+}
+let customerek = new Customer1("Ola", 22);   
 
 //obiekt utworzony przez new, 
 //przechouwje dane klasy, z której został utworzony
