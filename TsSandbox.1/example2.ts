bdc701eeba1961733c1848c8f4ff422fee508c74
Magda Wójcicka <wojcickam@tt.com.pl>
2018-08-02 10:21:50 +0200

    for of  vs. for in

--- TsSandbox.1/example2.ts
+++
@@ -1,5 +0,0 @@
-let a = 5;
-
-if (a > 10) {
-    console.log(a)
-}
---
+++ TsSandbox/example2.ts
@@ -0,0 +1,14 @@
+let a = 11;
+
+if (a > 10) {
+    console.log(a)
+}
+
+let numbers = [1,2,3,4,5,6,7,8,9]
+
+//for in to składnia do iterowania po obiektach
+//for of to składnia do iterowania po tablicach
+
+for (let n of numbers) {
+    console.log(n)
+}
