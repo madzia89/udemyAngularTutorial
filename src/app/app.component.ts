20e22a82db1ffae2d232e023c8bc95d29e70b0f0
Magda Wójcicka <wojcickam@tt.com.pl>
2018-08-03 13:06:36 +0200

    event binding na przykładzie zmiany koloru nagłówka

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,13 @@ export class AppComponent {
   nameColor: string = 'blue'
   photoUrl: string = "../assets/images/examplePicture.jpg"
   isActive: boolean = true
+  //czy dobrze rozumiem, że to co jest powyżej to stan wg react??????
 
   constructor(){
     
   }
-  changeIsActive(){
-    this.isActive = !this.isActive //przełączenie na przeciwną wartość (true/false)
+  changeColor(){
+    this.nameColor = this.nameColor === 'blue' ? 'red' : 'blue'
+    //zmiana koloru nagłówka w zależności od tego jaki kolor jest obecnie
   }
 }
