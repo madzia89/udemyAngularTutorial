b5c98d64b4ef0f665544fdd63919fb50b6bcbc4b
Magda Wójcicka <wojcickam@tt.com.pl>
2018-08-03 12:20:40 +0200

    nadanie koloru poprzez dobranie się do zagnieżdżonego pola obiektu style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   name: string = "Janusz Kowal"
+  nameColor: string = 'blue'
   photoUrl: string = "../assets/images/examplePicture.jpg"
 
   constructor(){
