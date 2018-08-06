0a5df44165437429acc9c40b2984082f9b94e496
Magda Wójcicka <wojcickam@tt.com.pl>
2018-08-06 09:51:33 +0200

    hostListener i wywołanie dyrektywy na zdarzenie np. mouseenter lub mouseleave

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
@@ -9,9 +9,21 @@ import { Directive, ElementRef } from '@angular/core';
 export class HighlightDirective {
 
   constructor(private element: ElementRef) { 
-    element.nativeElement.style.backgroundColor = "red"
-    //native element odowłuje się do faktycznego obiektu z drzewa html.
+  }
+ 
+  @HostListener('mouseenter') highlight(){
+    this.element.nativeElement.style.backgroundColor = "red"
+       //native element odowłuje się do faktycznego obiektu z drzewa html.
     //mając dostęp do tego obiektu możemy juz go np. stylować
+
+  
+    //tutaj potrzebne jest this.
+    // W zasięgu konsturktora czyli w funkcji powyżej
+    // mieliśmy bezpośredni dostępo do elementu, 
+    // tutaj musimy wskazać o jaki kontekst nam chodzi
   }
 
+  @HostListener('mouseleave') cancelHighlight(){
+    this.element.nativeElement.style.backgroundColor = null
+  }
 }
