c95db9e94366e4fe85f85194a8db8162d538178c
Magda Wójcicka <wojcickam@tt.com.pl>
2018-08-06 15:17:25 +0200

    fix SimpleChanges implementation error

--- src/app/customer-details/customer-details.component.ts
+++ src/app/customer-details/customer-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, EventEmitter, Output, OnDestroy, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output, OnDestroy, OnChanges } from '@angular/core';
 import { Customer, CustomerType } from '../model';
 
 @Component({
@@ -6,7 +6,8 @@ import { Customer, CustomerType } from '../model';
   templateUrl: './customer-details.component.html',
   styleUrls: ['./customer-details.component.css']
 })
-export class CustomerDetailsComponent implements OnInit, OnDestroy, OnChanges, SimpleChanges {
+
+export class CustomerDetailsComponent implements OnInit, OnDestroy, OnChanges {
 
   @Input() customer: Customer
   @Output() shift = new EventEmitter<string>()
@@ -26,16 +27,17 @@ export class CustomerDetailsComponent implements OnInit, OnDestroy, OnChanges, S
     // this.counterHandler = setInterval(()=>{this.counter++}, 1000)
   }
 
-  ngOnChanges(changes: SimpleChanges): void {
-    if (!changes.customer.firstChange) {
-      console.log(`change from ${changes.customer.previousValue.name} to ${changes.customer.currentValue.name}`)
-    }
-    //ngOnChanges daje możliwość odniesieia się do tego co działo się przed zmianą i co się dzieje teraz. 
-    //if na początku pozwala uniknąć błędu, który pojawiłby się gdybyśmy chcieli uzyskać dane z poprzedniej wartości gdy nią nie dysponujemy
-  } 
   ngOnDestroy() {
     console.log('exit')
     // clearInterval(this.counterHandler)
+  }
+
+  ngOnChanges(changes: any): void {
+    if (!changes.customer.firstChange){
+      console.log(`change from ${changes.customer.previousValue.name} to ${changes.customer.currentValue.name}`)
+    //ngOnChanges daje możliwość odniesieia się do tego co działo się przed zmianą i co się dzieje teraz. 
+    //if na początku pozwala uniknąć błędu, który pojawiłby się gdybyśmy chcieli uzyskać dane z poprzedniej wartości gdy nią nie dysponujemy
+    }
   }
 
   changeColor() {
